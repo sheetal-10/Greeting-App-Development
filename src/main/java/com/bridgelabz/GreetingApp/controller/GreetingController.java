ef90c6c92c4b711a36bd97b1e6db0fc76909ce60
sheetal-10 <sheetalp257@gmail.com>
2021-10-10 19:13:33 +0530

    Added: code to extend greeting controller to use greeting service to display hello message

--- src/main/java/com/bridgelabz/GreetingApp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/GreetingApp/controller/GreetingController.java
@@ -1,11 +1,16 @@
 package com.bridgelabz.GreetingApp.controller;
 
+import com.bridgelabz.GreetingApp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    @Autowired
+    GreetingService greetingService;
 
     @GetMapping
     public String greeting() {
@@ -16,4 +21,10 @@ public class GreetingController {
     public String greeting(@PathVariable String name) {
         return "Hello " + name + ". Welcome to our Greeting App ";
     }
+
+    @GetMapping("/greeting")
+    public String greetingService(){
+        return greetingService.helloGreeting();
+    }
+
 }
