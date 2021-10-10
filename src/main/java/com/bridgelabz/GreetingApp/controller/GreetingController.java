98f0f6312aa9829a5ac1f16e674afe6a5514f1bf
sheetal-10 <sheetalp257@gmail.com>
2021-10-10 19:50:38 +0530

    Added: method to get greeting message based on user provided atrributes

--- src/main/java/com/bridgelabz/GreetingApp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/GreetingApp/controller/GreetingController.java
@@ -4,6 +4,7 @@ import com.bridgelabz.GreetingApp.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,8 +23,13 @@ public class GreetingController {
         return "Hello " + name + ". Welcome to our Greeting App ";
     }
 
+    @GetMapping(value = "/greeting1")
+    public String greeting1(@RequestParam String fname, @RequestParam String lname) {
+        return "Hello : " + fname + " " + lname;
+    }
+
     @GetMapping("/greeting")
-    public String greetingService(){
+    public String greetingService() {
         return greetingService.helloGreeting();
     }
 
